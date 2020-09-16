7f526c345db8c53f34965789d7f19dc13c32ae5b
AbdullahMoawad <effinbzz@gmail.com>
2020-09-16 03:38:21 +0200

    test commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function Login(x){
+    if (x >= 0){
+        return x;
+    }else{
+        return -x;
+    }
+}
+
+Login(-10) === Login(-10)
+
+function sum(){
+    let sum = 0;
+
+    for (let x of array ){
+        sum += x;
+    }
+    return sum;
+}
