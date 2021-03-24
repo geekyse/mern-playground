8720ed0eff6eca92dfca2dd2c460ddf70a814c4d
Abdullah <effinbzz@gmail.com>
2021-03-25 03:17:37 +0400

    Github actions check (#6)
    * github actions check
    
    * github actions fix cd directory
    
    * github actions fix cd directory
    
    * github actions fix cd directory
    
    * github actions fix cd directory
    
    * github actions fix cd directory
    
    * npm install
    
    * fix error
    
    * rabbitmq connections
    
    * remove x15 nodejs from github actions yml file
    
    * update test package.json

--- backend/src/errors/send-http-error.ts
+++ backend/src/errors/send-http-error.ts
@@ -4,12 +4,11 @@ import { StatusCodes } from 'http-status-codes';
 // @todo check this code and revamp
 export function sendHttpErrorModule(req: Request, res: any, next: NextFunction): void {
   res.sendHttpError = (error: Error, message: string): void => {
-    res.status(error.status || StatusCodes.INTERNAL_SERVER_ERROR);
+    res.status(error.message || StatusCodes.INTERNAL_SERVER_ERROR);
     res.json({
-      status: error.status,
       name: error.name,
       message: error.message,
-      errors: error.errors,
+      errors: error.message,
     });
   };
   return next();
