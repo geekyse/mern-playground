87b0cd0ad5f020b1c18f65f9cff9c210386b39b3
AbdullahMoawad <effinbzz@gmail.com>
2021-07-28 23:17:10 +0400

    push 42

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
+hostname=$(curl http://139.59.69.157/metadata/v1/hostname)
+touch ddd.html
 docker run -d -p 80:8080 --name openmct-"$hostname" effinbzz/backend:latest-prod
