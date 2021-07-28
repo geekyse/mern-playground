ae15be54b69ff0ebb9e7dad305da86f33b8d92cb
AbdullahMoawad <effinbzz@gmail.com>
2021-07-28 22:56:47 +0400

    push 37

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-hostname=$(curl http://169.254.169.254/metadata/v1/user-data)
-docker run -d -p 80:8080 --name openmct-"$hostname" Ec-App/backend:"$hostname"
+#!/bin/bash
+hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
+docker run -d -p 80:8080 --name openmct-"$hostname" effinbzz/backend:latest-prod
