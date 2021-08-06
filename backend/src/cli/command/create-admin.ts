49bb1c2e25e8c7d1d7876e8e33771d57e92ea990
AbdullahMoawad <effinbzz@gmail.com>
2021-08-06 21:41:54 +0400

    Fix cli command to creat admin user

--- backend/src/cli/command/create-admin.ts
+++ backend/src/cli/command/create-admin.ts
@@ -1,26 +1,27 @@
-// import { IUser, User } from '../../models/User';
-//
-// const createAdmin = async () => {
-//   const user: IUser = new User();
-//
-//   user.userName = 'effinbzz';
-//   user.firstName = 'Abdullah';
-//   user.lastName = 'Moawad';
-//   user.email = 'effinbzz@gmail.com';
-//   user.password = '123123123';
-//   user.role = 'owner';
-//
-//   const userRow = await (new User).createUser(user);
-//
-//   if (userRow) {
-//     console.log('user created');
-//     console.log(userRow.id);
-//   } else {
-//     console.error('error');
-//     console.log(userRow);
-//   }
-//
-//   process.exit();
-// };
-//
-// export { createAdmin };
+import { User } from '../../models/User';
+
+const createAdmin = async () => {
+
+  const user = {
+    userName: 'effinbzz',
+    firstName: 'Abdullah',
+    lastName: 'Moawad',
+    email: 'effinbzz@gmail.com',
+    password: '123123123',
+    role: 'admin',
+  };
+
+  const userRow = await  User.create(user);
+
+  if (userRow) {
+    console.log('user created');
+    console.log(userRow.id);
+  } else {
+    console.error('error');
+    console.log(userRow);
+  }
+
+  process.exit();
+};
+
+export { createAdmin };
